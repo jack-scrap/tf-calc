78bef57cdb62e941131baf0f4cec7ff0c793ec9a
Jack <jackhasakeyboard@gmail.com>
2022-07-30 03:02:53 -0600

    Added enter key

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	for (let i = 0; i < 1 + 9; i++) {
 		$("#keyPad").append(`<div class="btn">${i}</div>`);
 	}
+
+	$("#keyPad").append("<div class='btn'>Enter</div>");
 });
