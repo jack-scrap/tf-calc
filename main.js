fa5440f9cfc6461f3b5c6bceb9bb05a872774506
Jack <jackhasakeyboard@gmail.com>
2022-07-31 02:01:01 -0600

    Moved utility to parse into parsing library

--- main.js
+++ main.js
@@ -6,55 +6,6 @@ function drawScr() {
 	$("#scr").text(buff);
 }
 
-function eval(tok) {
-	let res = tok[0];
-
-	if (!tok.length) {
-		return err("No tokens");
-	}
-
-	if (tok.length > 1 && !(tok.length % 2)) {
-		return err(`Inappropriate number of tokens (${tok.length})`);
-	}
-
-	let i = 1;
-	while (i < tok.length - 1) {
-		if (typeof tok[i] != "string") {
-			return err(`Unexpected token '${tok[i]}' at position ${i}`);
-		}
-
-		if (typeof tok[i + 1] != "number") {
-			return err(`Unexpected token '${tok[i + 1]}' at position ${i + 1}`);
-		}
-
-		switch (tok[i]) {
-			case "+":
-				res += tok[i + 1];
-
-				break;
-
-			case "-":
-				res -= tok[i + 1];
-
-				break;
-
-			case "*":
-				res *= tok[i + 1];
-
-				break;
-
-			case "/":
-				res *= tok[i + 1];
-
-				break;
-		}
-
-		i += 1 + 1;
-	}
-
-	return res;
-}
-
 $(document).ready(function() {
 	// screen
 	drawScr();
