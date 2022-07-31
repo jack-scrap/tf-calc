c3d36e574620bd849f1dfe8298cb68da884f9b36
Jack <jackhasakeyboard@gmail.com>
2022-07-31 01:12:08 -0600

    Implemented metal keys by inserting numeric value of metal

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
 	}
 	$("#no").append(`<div class="btn no">${0}</div>`);
 
-	$("#metal").append("<div class='btn metal'><img src='res/icon/scrap.png' /></div>");
-	$("#metal").append("<div class='btn metal'><img src='res/icon/rec.png' /></div>");
-	$("#metal").append("<div class='btn metal'><img src='res/icon/ref.png' /></div>");
+	$("#metal").append("<div class='btn metal' id='scrap'><img src='res/icon/scrap.png' /></div>");
+	$("#metal").append("<div class='btn metal' id='rec'><img src='res/icon/rec.png' /></div>");
+	$("#metal").append("<div class='btn metal' id='ref'><img src='res/icon/ref.png' /></div>");
 
 	$("#no").append("<div class='btn' id='enter'>Enter</div>");
 	$("#no").append("<div class='btn' id='clr'>CLR</div>");
@@ -25,6 +25,24 @@ $(document).ready(function() {
 	// ctrl
 	$(".no").mousedown(function() {
 		tok.push($(this).text());
+
+		drawScr();
+	});
+
+	$("#scrap").mousedown(function() {
+		tok.push(0.11);
+
+		drawScr();
+	});
+
+	$("#rec").mousedown(function() {
+		tok.push(0.33);
+
+		drawScr();
+	});
+
+	$("#ref").mousedown(function() {
+		tok.push(1.0);
 
 		drawScr();
 	});
